c6dbe79cafa35a38440f8c883c29b308455d7f6a
gumtrip <gumtrip@126.com>
2020-05-21 15:30:24 +0800

    修改seo

--- seo.config.js
+++ seo.config.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   APP_URL: 'https://milesolar.com',
-  TITLE: 'Power inverter, MPPT controller, off-grid solar system, MILESOLAR',
-  KEYWORDS: 'manufacturing solar inverters, MPPT solar controllers,off grid solar system',
-  DESCRIPTION: 'MILESOLAR specializes in the manufacturing of power inverters, solar inverters, MPPT solar controllers, solar lights, and more. We provide professional off-grid solar powered solutions'
+  TITLE: 'power inverters, MPPT controller, off-grid solar system, MILESOLAR',
+  KEYWORDS: ' power inverters, solar inverters, MPPT solar controllers, solar generators, off-grid solar system',
+  DESCRIPTION: 'MILESOLAR specializes in the manufacturing of power inverters, solar inverters, MPPT solar controllers, solar generators and solar power system products'
 }
